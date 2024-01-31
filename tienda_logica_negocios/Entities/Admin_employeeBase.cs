18f4d1ab6458e7e90dbf093a755633d281abf258
walter guaraca <130495513+walter04guarac@users.noreply.github.com>
2024-01-31 07:46:10 -0500

    hello

--- tienda_logica_negocios/Entities/Admin_employeeBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace tienda_logica_negocios.Entities
-{
-    internal class Admin_employeeBase
-    {
-        public string Cargo;
-        public int Id;
-        public string Nombre;
-    }
-}
